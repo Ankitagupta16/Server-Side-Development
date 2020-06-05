13416bc02c6a7757b3478cc4d559e335a4c48b27
Ankita Gupta <1610ankita.gupta@gmail.com>
2020-06-05 12:45:31 +0530

    passport facebook

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -5,6 +5,8 @@ var User = require('./models/user');
 var JwtStrategy = require('passport-jwt').Strategy;//provide strategy for configuring token
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var FacebookTokenStrategy = require('passport-facebook-token');
+
 
 var config = require('./config.js');
 
@@ -57,4 +59,36 @@ exports.verifyAdmin = function(req,res,next){
         err.status = 403;
         return next(err);
     }
+};
+
+//facebook token strategy 
+exports.facebookPassport = passport.use(new 
+FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        //there is an error
+        if (err) {
+            return done(err, false);
+        }
+        //user has already logged in 
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        //user dosent exit: create new user
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
 }
+));
--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -3,5 +3,9 @@
 
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2600126763594653',
+        clientSecret: '74b153f65a34085828f2fd3d4a0589b5'
+    }
 }
